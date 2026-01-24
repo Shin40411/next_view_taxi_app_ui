b0a521b407b5283549fbcaf437a5c7af65197f5d
Dang Tho <dtho961@gmail.com>
2026-01-24 11:27:51 +0700

    feat: add sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import checker from 'vite-plugin-checker';
+import sitemap from 'vite-plugin-sitemap';
 
 // ----------------------------------------------------------------------
 
@@ -13,6 +14,18 @@ export default defineConfig({
       // eslint: {
       //   lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"',
       // },
+    }),
+    sitemap({
+      hostname: 'https://goxu.vn',
+      dynamicRoutes: [
+        '/',
+        '/auth/jwt/login',
+        '/auth/jwt/register',
+        '/auth/jwt/forgot-password',
+        '/auth/jwt/reset-password',
+        '/auth/jwt/verify-otp',
+        '/auth/jwt/request-login-otp',
+      ],
     }),
   ],
   resolve: {
